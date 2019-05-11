2da5961b748c42fcad0ef7be9e94eed33b1822d5
miguelagol <michalina.gol@gmail.com>
2019-05-11 14:47:11 +0200

    Style booking steps

--- src/components/BookingSteps/BookingSteps.js
+++ src/components/BookingSteps/BookingSteps.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import css from './BookingSteps.module.scss';
 
 export default function BookingSteps() {
    const steps = ['Where & When', 'Choose a Car', 'Details & Payment'];
 
-   return steps.map((step, index) => (
-      <div key={index}>
-         <div>{index + 1}</div>
-         <div>{step}</div>
+   return (
+      <div className={css['booking-steps']}>
+         {steps.map((step, index) => (
+            <div key={step} className={css['booking-step']}>
+               <div className={css['step-number']}>{index + 1}</div>
+               <div className={css['step-label']}>{step}</div>
+            </div>
+         ))}
       </div>
-   ));
+   );
 }
