02f955e37e68218e7cd64ca3114d600224440210
miguelagol <michalina.gol@gmail.com>
2019-05-11 15:52:13 +0200

    Style active booking step

--- src/components/BookingSteps/BookingSteps.js
+++ src/components/BookingSteps/BookingSteps.js
@@ -1,17 +1,27 @@
 import React from 'react';
+import classnames from 'classnames/bind';
 import css from './BookingSteps.module.scss';
+
+const classNames = classnames.bind(css);
 
 export default function BookingSteps() {
    const steps = ['Where & When', 'Choose a Car', 'Details & Payment'];
 
+   const activeStep = 2;
+
+   const stepsClasses = classNames('booking-steps', { [`booking-steps--${activeStep}`]: activeStep + 1 });
+
    return (
-      <div className={css['booking-steps']}>
-         {steps.map((step, index) => (
-            <div key={step} className={css['booking-step']}>
-               <div className={css['step-number']}>{index + 1}</div>
-               <div className={css['step-label']}>{step}</div>
-            </div>
-         ))}
+      <div className={stepsClasses}>
+         {steps.map((step, index) => {
+            const stepClasses = classNames('booking-step', { 'active-step': index === activeStep });
+            return (
+               <div key={step} className={stepClasses}>
+                  <div className={css['step-number']}>{index + 1}</div>
+                  <div className={css['step-label']}>{step}</div>
+               </div>
+            );
+         })}
       </div>
    );
 }
