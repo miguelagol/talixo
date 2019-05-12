7859ce4a36bbd25da7315961918d31a1a04d77c7
miguelagol <michalina.gol@gmail.com>
2019-05-12 22:32:03 +0200

    Add all reusable components in App.js for testing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import css from './App.module.scss';
 import logo from './images/logo.png';
 import BookingSteps from './components/BookingSteps';
 import Button from './components/Button';
+import LabelContainer from './components/LabelContainer';
+import Input from './components/Input';
+import Select from './components/Select';
+import DatePicker from './components/DatePicker';
+import { FaBaby as BabyIcon } from 'react-icons/fa';
 
 function App() {
    return (
@@ -10,9 +15,34 @@ function App() {
          <header className={css.header}>
             <img src={logo} alt="Talixo logo" className={css['talixo-logo']} />
          </header>
+
          <div className={css['content-container']}>
-            <BookingSteps activeStep={3} />
+            <BookingSteps activeStep={1} />
+            <br />
+            <br />
+            <LabelContainer labelPosition="left" label="Voucher code (optional):">
+               <Input />
+            </LabelContainer>
+            <br />
+            <LabelContainer labelPosition="top" label="Voucher code (optional):">
+               <Input />
+            </LabelContainer>
+            <br />
+            <br />
+            <LabelContainer labelPosition="left" label={<BabyIcon style={{ padding: '0 8px' }} />} variant="dark">
+               <Select placeholder="e.g Torstrasse 124, Berlin" />
+            </LabelContainer>
+            <br />
+            <LabelContainer labelPosition="top" label="Pick up:">
+               <Select placeholder="e.g Torstrasse 124, Berlin" />
+            </LabelContainer>
+            <br />
             <Button type="button">Start Booking</Button>
+            <br />
+            <br />
+            <LabelContainer labelPosition="left" label="Pick up:">
+               <DatePicker value={new Date('05 12 2019 8:00 PM')} />
+            </LabelContainer>
          </div>
       </div>
    );
