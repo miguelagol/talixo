e09b1914b83c0405ef4cb7aad20ec7f188030466
miguelagol <michalina.gol@gmail.com>
2019-05-12 09:22:33 +0200

    Styled select input

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -3,11 +3,33 @@ import AsyncSelect from 'react-select/lib/Async';
 
 export default function Select({ ...rest }) {
    const selectStyles = {
-      control: styles => ({ ...styles, border: 0, height: '48px', ' > div': { padding: 0 } }),
-      placeholder: styles => ({ ...styles, color: '#d2d6da', ' + div': { margin: 0, padding: 0 } }),
+      control: styles => ({ ...styles, border: 0 }),
+      placeholder: styles => ({
+         ...styles,
+         color: '#d2d6da',
+         fontSize: 20,
+         margin: 0,
+         ' + div': { margin: 0, padding: 0 },
+      }),
       indicatorsContainer: styles => ({
          ...styles,
-         svg: { width: '30px', height: '30px', color: '#ff4800' },
+         svg: { width: 32, height: 32, color: '#ff4800' },
+      }),
+      valueContainer: styles => ({
+         ...styles,
+         padding: '0px 8px',
+      }),
+      input: styles => ({
+         ...styles,
+         fontSize: 20,
+      }),
+      singleValue: styles => ({
+         ...styles,
+         fontSize: 20,
+      }),
+      option: styles => ({
+         ...styles,
+         color: 'black',
       }),
    };
 
