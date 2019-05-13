4388e728df2b0d9c9ea8b529aec4d1c64409daa7
miguelagol <michalina.gol@gmail.com>
2019-05-13 18:02:54 +0200

    Adjust DayPicker styles

--- src/components/DayPicker/DayPicker.js
+++ src/components/DayPicker/DayPicker.js
@@ -1,23 +1,28 @@
 import React from 'react';
+import classnames from 'classnames/bind';
 import css from './DayPicker.module.scss';
 
-export default function DaysContainer(date) {
+const classNames = classnames.bind(css);
+
+export default function DaysContainer() {
+   const labelClasses = classNames('day-container', 'active');
+
    return (
       <div className={css['days-container']}>
-         <input type="radio" id="day1" name="radios" checked />
-         <label htmlFor="day1" className={css['day-container']}>
+         <label className={labelClasses}>
+            <input type="radio" id="day1" name="radios" />
             <span className={css.date}>13.05</span>
             <span className={css.day}>Today</span>
          </label>
 
-         <input type="radio" id="day2" name="radios" />
-         <label htmlFor="day2" className={css['day-container']}>
+         <label className={css['day-container']}>
+            <input type="radio" id="day2" name="radios" />
             <span className={css.date}>14.05</span>
             <span className={css.day}>Tomorrow</span>
          </label>
 
-         <input type="radio" id="day3" name="radios" />
-         <label htmlFor="day3" className={css['day-container']}>
+         <label className={css['day-container']}>
+            <input type="radio" id="day3" name="radios" />
             <span className={css.date}>15.05</span>
             <span className={css.day}>Wednesday</span>
          </label>
