180f273b9554cfd9ef95f7b45d49a3e77686c8ce
miguelagol <michalina.gol@gmail.com>
2019-05-13 10:28:29 +0200

    Add main form layout

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import css from './App.module.scss';
 import logo from './images/logo.png';
 import BookingSteps from './components/BookingSteps';
 import DayPicker from './components/DayPicker';
+import Button from './components/Button';
+import LabelContainer from './components/LabelContainer';
+import Input from './components/Input';
+import Select from './components/Select';
+import DatePicker from './components/DatePicker';
+import { FaBaby as BabyIcon } from 'react-icons/fa';
+import { FaDog as DogIcon } from 'react-icons/fa';
+import { FaSuitcase as SuitcaseIcon } from 'react-icons/fa';
+import { FaSkiingNordic as SportIcon } from 'react-icons/fa';
+import { IoIosPerson as PersonIcon } from 'react-icons/io';
 
 function App() {
    return (
@@ -18,7 +28,53 @@ function App() {
             <BookingSteps activeStep={1} />
             <DayPicker date={new Date()} />
             <br />
-            <br />
+
+            <LabelContainer labelPosition="top" label="Pick up:">
+               <Select placeholder="e.g Torstrasse 124, Berlin" />
+            </LabelContainer>
+
+            <LabelContainer labelPosition="top" label="Destination:">
+               <Select placeholder="e.g Tegel Airport" />
+            </LabelContainer>
+
+            <LabelContainer labelPosition="top" label="On:">
+               <DatePicker value={new Date('05 12 2019 8:00 PM')} />
+            </LabelContainer>
+
+            <LabelContainer labelPosition="top" label="Voucher code (optional):">
+               <Input />
+            </LabelContainer>
+
+            <div className={css['options-container']}>
+               <span>For 1-2 passengers with 1-2 bags</span>
+               <span>more options</span>
+
+               <LabelContainer labelPosition="left" label={<PersonIcon style={{ padding: '0 8px' }} />} variant="dark">
+                  <Select placeholder="2" />
+               </LabelContainer>
+
+               <LabelContainer
+                  labelPosition="left"
+                  label={<SuitcaseIcon style={{ padding: '0 8px' }} />}
+                  variant="dark"
+               >
+                  <Select placeholder="2" />
+               </LabelContainer>
+
+               <LabelContainer labelPosition="left" label={<SportIcon style={{ padding: '0 8px' }} />} variant="dark">
+                  <Select placeholder="2" />
+               </LabelContainer>
+
+               <LabelContainer labelPosition="left" label={<DogIcon style={{ padding: '0 8px' }} />} variant="dark">
+                  <Select placeholder="2" />
+               </LabelContainer>
+
+               <LabelContainer labelPosition="left" label={<BabyIcon style={{ padding: '0 8px' }} />} variant="dark">
+                  <Select placeholder="2" />
+               </LabelContainer>
+            </div>
+
+            <Button type="button">Start Booking</Button>
          </div>
       </div>
    );
