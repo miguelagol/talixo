c99e86a2cd15d86ff46dcfc053b4d7d821cdd8fa
miguelagol <michalina.gol@gmail.com>
2019-05-15 17:37:07 +0200

    Add horizontal version of booking summary

--- src/components/BookingSummary/BookingSummary.js
+++ src/components/BookingSummary/BookingSummary.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { format } from 'date-fns';
 import PropTypes from 'prop-types';
+import classnames from 'classnames/bind';
 import css from './BookingSummary.module.scss';
 
-export default function BookingSummary({ formState }) {
+const classNames = classnames.bind(css);
+
+export default function BookingSummary({ formState, variant }) {
    const {
       startAddress,
       endAddress,
@@ -16,48 +19,59 @@ export default function BookingSummary({ formState }) {
       childrenSeats,
    } = formState;
 
+   const summaryListClasses = classNames('summary-list', { [variant]: variant });
+
    return (
       <div className={css.summary}>
          <h2 className={css.header}>Your booking</h2>
-         <div className={css['summary-list']}>
-            <div className={css.label}>From:</div> <div>{startAddress.label}</div>
-            <div className={css.label}>To: </div>
-            <div>{endAddress.label}</div>
-            <div className={css.label}>Date:</div>
-            <div>
-               {format(date, 'dddd')}, {format(date, 'DD MMMM YYYY')}
+         <div className={summaryListClasses}>
+            <div className={css['booking-value']}>
+               <div className={css.label}>From:</div>
+               <div>{startAddress.label}</div>
             </div>
-            <div className={css.label}>Pickup time:</div>
-            <div>{format(date, 'HH:mm')}</div>
+            <div className={css['booking-value']}>
+               <div className={css.label}>To: </div>
+               <div>{endAddress.label}</div>
+            </div>
+            <div className={css['booking-value']}>
+               <div className={css.label}>Date:</div>
+               <div>
+                  {format(date, 'dddd')}, {format(date, 'DD MMMM YYYY')}
+               </div>
+            </div>
+            <div className={css['booking-value']}>
+               <div className={css.label}>Pickup time:</div>
+               <div>{format(date, 'HH:mm')}</div>
+            </div>
             {passengers && (
-               <>
+               <div className={css['booking-value']}>
                   <div className={css.label}>Passengers:</div> <div>{passengers.label}</div>
-               </>
+               </div>
             )}
             {suitcase !== undefined && suitcase.label !== 0 && (
-               <>
+               <div className={css['booking-value']}>
                   <div className={css.label}>Luggage:</div> <div>{suitcase.label}</div>
-               </>
+               </div>
             )}
             {sportLuggage !== undefined && sportLuggage.label !== 0 && (
-               <>
+               <div className={css['booking-value']}>
                   <div className={css.label}>Sport luggage:</div> <div>{sportLuggage.label}</div>
-               </>
+               </div>
             )}
             {pets !== undefined && pets.label !== 0 && (
-               <>
+               <div className={css['booking-value']}>
                   <div className={css.label}>Animals:</div> <div>{pets.label}</div>
-               </>
+               </div>
             )}
             {childrenSeats !== undefined && childrenSeats.label !== 0 && (
-               <>
+               <div className={css['booking-value']}>
                   <div className={css.label}>Children seats:</div> <div>{childrenSeats.label}</div>
-               </>
+               </div>
             )}
             {voucherCode !== undefined && (
-               <>
+               <div className={css['booking-value']}>
                   <div className={css.voucherLabel}>Voucher code:</div> <div>{voucherCode}</div>
-               </>
+               </div>
             )}
          </div>
       </div>
@@ -66,4 +80,9 @@ export default function BookingSummary({ formState }) {
 
 BookingSummary.propTypes = {
    formState: PropTypes.object.isRequired,
+   variant: PropTypes.string,
+};
+
+BookingSummary.defaultProps = {
+   variant: undefined,
 };
