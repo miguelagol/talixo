99a0ec7fb3255bc15c8fa2d9df5714a85be21a57
miguelagol <michalina.gol@gmail.com>
2019-05-15 16:26:32 +0200

    Add second form layout

---
+++ src/data/carsData.js
@@ -0,0 +1,47 @@
+export const carsOptions = [
+   {
+      url: 'https://static.talixo.de/images/vehicles/economy.png',
+      title: 'Economy+',
+      brands: ['Toyota Prius', 'Audi A4'],
+      features: ['Meet & Greet', '15 minutes waiting time', 'Fixed price - no traffic charges'],
+      price: '184.00',
+   },
+   {
+      url: 'https://static.talixo.de/images/vehicles/economy_van.png',
+      title: 'Economy VAN',
+      brands: ['Mercedes Vito', 'VW T5'],
+      features: ['Meet & Greet', '15 minutes waiting time', 'Fixed price - no traffic charges'],
+      price: '184.00',
+   },
+   {
+      url: 'https://static.talixo.de/images/vehicles/business.png',
+      title: 'Business Sedan',
+      brands: ['Mercedes E-Class', 'Audi A6', 'BMW 5'],
+      features: [
+         'Black business limousine',
+         'Meet & Greet',
+         '15 minutes waiting time',
+         'Fixed price - no traffic charges',
+      ],
+      price: '309.44',
+   },
+   {
+      url: 'https://static.talixo.de/images/vehicles/business_van.png',
+      title: 'Business VAN',
+      brands: ['Mercedes Viano', 'Mercedes V-Class'],
+      features: [
+         'Black business limousine',
+         'Meet & Greet',
+         '15 minutes waiting time',
+         'Fixed price - no traffic charges',
+      ],
+      price: '496.51',
+   },
+   {
+      url: 'https://static.talixo.de/images/vehicles/first.png',
+      title: 'First Class',
+      brands: ['Mercedes S-Class', 'Audi A8', 'BMW 7'],
+      features: ['Black luxury limousine', 'Meet & Greet', '30 minutes waiting time', 'Bottled water'],
+      price: '681.06',
+   },
+];
