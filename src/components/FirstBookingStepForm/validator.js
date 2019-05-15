c13e1cd77d0f40bce1862e667ed73cf0b96b4a2a
miguelagol <michalina.gol@gmail.com>
2019-05-15 17:34:38 +0200

    Add new validation

--- src/components/FirstBookingStepForm/validator.js
+++ src/components/FirstBookingStepForm/validator.js
@@ -19,5 +19,11 @@ export default function validator(values) {
    ) {
       errors.voucherCode = 'Invalid voucher code';
    }
+
+   if (values.startAddress === values.endAddress && values.startAddress && values.endAddress) {
+      errors.startAddress = 'Change the starting address. (It must be different from the final one)';
+      errors.endAddress = 'Change the final address. (It must be different from the starting one)';
+   }
+
    return errors;
 }
